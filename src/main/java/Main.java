04fd4349fd8be05c22d8aa7326a5ff38f68d8def
Dennis <coonh@kabelmail.de>
2019-10-15 14:38:23 +0200

    main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class main extends Application {
+public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
