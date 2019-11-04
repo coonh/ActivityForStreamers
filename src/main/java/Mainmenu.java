ea547d517bb1c6e111a31ccfa80777ed7b502476
Dennis <coonh@kabelmail.de>
2019-11-04 09:37:10 +0100

    You now can press Enter in the Mainmenu to press send Button

--- src/main/java/Mainmenu.java
+++ src/main/java/Mainmenu.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -103,6 +104,13 @@ public class Mainmenu {
             g.show(stage);
         });
 
+
+        v.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.ENTER) {
+                System.out.println("Wir drücken den Knopf");
+                send.fire();
+            }
+        });
         scene = new Scene(v);
     }
 
